c3439abc3e5b9f2660b5fda200e6e5f8f45692ef
panfengfeng <panfengfeng@ncic.ac.cn>
2016-08-24 11:16:10 +0800

    modify test_table_reader_bench.sh

--- test_table_reader_bench.sh
+++ test_table_reader_bench.sh
@@ -4,14 +4,14 @@
 echo 3 > /proc/sys/vm/drop_caches
 
 #terarkzip table
-#./table_reader_bench --table_factory=terark_zip --through_db=false
+./table_reader_bench --table_factory=terark_zip --through_db=false --iterator=false
 
 #plain_table
-#./table_reader_bench --table_factory=plain_table --through_db=false 
+#./table_reader_bench --table_factory=plain_table --through_db=false --iterator=false  
 
 #cuckoo_table
-#./table_reader_bench --table_factory=cuckoo_hash --through_db=false
+#./table_reader_bench --table_factory=cuckoo_hash --through_db=false --iterator=false
 
 #block_based table
-./table_reader_bench --table_factory=block_based --through_db=false
+#./table_reader_bench --table_factory=block_based --through_db=false --iterator=false
 

24552e3575d758ef869d411bb623ae475471bea3
terark <terark@localhost.localdomain>
2017-08-22 20:52:18 +0800

    Fix MergingIterator::AddIterator bug

--- table/merging_iterator.cc
+++ table/merging_iterator.cc
@@ -57,17 +57,18 @@ class MergingIterator : public InternalIterator {
   }
 
   virtual void AddIterator(InternalIterator* iter) {
-    abort(); // should not be called
     assert(direction_ == kForward);
     children_.emplace_back(iter);
     if (pinned_iters_mgr_) {
       iter->SetPinnedItersMgr(pinned_iters_mgr_);
     }
-    auto new_wrapper = children_.back();
-    if (new_wrapper.Valid()) {
-      minHeap_.push(&new_wrapper);
-      current_ = CurrentForward();
+    minHeap_.clear();
+    for (auto& child : children_) {
+      if (child.Valid()) {
+        minHeap_.push(&child);
+      }
     }
+    current_ = CurrentForward();
   }
 
   virtual ~MergingIterator() {

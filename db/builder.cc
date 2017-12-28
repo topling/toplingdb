0fcb704d9c2cb493d52846f6aac3942b214fcdc0
rockeet <rockeet@gmail.com>
2017-12-28 11:53:13 +0800

    db/builder.cc: fix bug on code merge

--- db/builder.cc
+++ db/builder.cc
@@ -142,7 +142,7 @@ Status BuildTable(
         true /* internal key corruption is not ok */, range_del_agg.get());
     CompactionIterator c_iter2(
         iter, internal_comparator.user_comparator(), &merge2, kMaxSequenceNumber,
-        &snapshots, earliest_write_conflict_snapshot, env,
+        &snapshots, earliest_write_conflict_snapshot, snapshot_checker, env,
         true /* internal key corruption is not ok */, range_del_agg.get());
     auto second_pass_iter = c_iter2.AdaptToInternalIterator();
     builder->SetSecondPassIterator(second_pass_iter.get());

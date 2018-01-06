a6c0edfcc776fca6cfa27a5585ce96dab32ab132
rockeet <rockeet@163.com>
2018-01-06 14:19:09 +0800

    db/compaction_picker_universal.cc: do not pick if too bad

--- db/compaction_picker_universal.cc
+++ db/compaction_picker_universal.cc
@@ -591,6 +591,7 @@ Compaction* UniversalCompactionPicker::PickCompactionToReduceSortedRuns(
                        cf_name.c_str(), file_num_buf, loop);
     }
 
+    uint64_t max_sr_size = candidate_size;
     // Check if the succeeding files need compaction.
     for (size_t i = loop + 1;
          candidate_count < max_files_to_compact && i < sorted_runs.size();
@@ -613,6 +614,7 @@ Compaction* UniversalCompactionPicker::PickCompactionToReduceSortedRuns(
       if (sz < static_cast<double>(succeeding_sr->size)) {
         break;
       }
+      max_sr_size  = std::max(max_sr_size, succeeding_sr->compensated_file_size);
       if (ioptions_.compaction_options_universal.stop_style ==
           kCompactionStopStyleSimilarSize) {
         // Similar-size stopping rule: also check the last picked file isn't
@@ -634,9 +636,35 @@ Compaction* UniversalCompactionPicker::PickCompactionToReduceSortedRuns(
 
     // Found a series of consecutive files that need compaction.
     if (candidate_count >= (unsigned int)min_merge_width) {
-      start_index = loop;
-      done = true;
-      break;
+      bool pick = true;
+      if (kCompactionStopStyleTotalSize ==
+          ioptions_.compaction_options_universal.stop_style) {
+        double sum_sr_size  = candidate_size;
+        double max_sr_ratio = double(max_sr_size) / sum_sr_size;
+        auto& o = mutable_cf_options;
+        if ((max_sr_ratio < 0.60) ||
+            (max_sr_ratio < 0.80 &&
+             candidate_size < o.write_buffer_size * o.max_write_buffer_number
+            )) {
+          // not so bad, pick the compaction
+        }
+        else {
+          pick = false; // too bad, don't pick the compaction
+          for (size_t i = loop;
+               i < loop + candidate_count && i < sorted_runs.size(); i++) {
+            const SortedRun* skipping_sr = &sorted_runs[i];
+            char file_num_buf[256];
+            skipping_sr->DumpSizeInfo(file_num_buf, sizeof(file_num_buf), loop);
+            ROCKS_LOG_BUFFER(log_buffer, "[%s] Universal: max_sr_ratio = %7.5f too big, skipping %s",
+                             cf_name.c_str(), max_sr_ratio, file_num_buf);
+          }
+        }
+      }
+      if (pick) {
+        start_index = loop;
+        done = true;
+        break;
+      }
     } else {
       for (size_t i = loop;
            i < loop + candidate_count && i < sorted_runs.size(); i++) {

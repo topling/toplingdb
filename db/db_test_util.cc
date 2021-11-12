34e07b74ddbecaaa4b39a860f8616f9fe66bd425
leipeng <rockeet@gmail.com>
2021-11-12 18:29:03 +0800

    remove code: system(("mkdir -p " + dbname_).c_str());

--- db/db_test_util.cc
+++ db/db_test_util.cc
@@ -711,7 +711,6 @@ Status DBTestBase::TryReopen(const Options& options) {
   // clears the block cache.
   last_options_ = options;
   MaybeInstallTimeElapseOnlySleep(options);
-  system(("mkdir -p " + dbname_).c_str());
   return DB::Open(options, dbname_, &db_);
 }
 

91d5996c45ac2b0979015873514a22b3b6bf34f5
leipeng <peng@topling.cn>
2023-02-04 12:18:01 +0800

    Slice: Add `<=` and `>=`

--- include/rocksdb/slice.h
+++ include/rocksdb/slice.h
@@ -281,6 +281,8 @@ inline bool operator<(const Slice& x, const Slice& y) {
     return x.size_ < y.size_;
 }
 inline bool operator>(const Slice& x, const Slice& y) { return y < x; }
+inline bool operator>=(const Slice& x, const Slice& y) { return !(x < y); }
+inline bool operator<=(const Slice& x, const Slice& y) { return !(y < x); }
 
 inline std::string operator+(const Slice& x, const Slice& y) {
   std::string z; z.reserve(x.size_ + y.size_);

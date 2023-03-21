9508a6bc1a4761a272d9d6ce2e03d11d5bc8f2d5
leipeng <peng@topling.cn>
2023-03-21 22:01:39 +0800

    Update Copyright for SideGetBenchmarks.java

--- java/jmh/src/main/java/org/rocksdb/jmh/SideGetBenchmarks.java
+++ java/jmh/src/main/java/org/rocksdb/jmh/SideGetBenchmarks.java
@@ -1,8 +1,12 @@
 /**
- * Copyright (c) 2011-present, Facebook, Inc.  All rights reserved.
+ * Copyright (c) 2023-present, Topling, Inc.  All rights reserved.
  * This source code is licensed under both the GPLv2 (found in the
  * COPYING file in the root directory) and Apache 2.0 License
  * (found in the LICENSE.Apache file in the root directory).
+ */
+/**
+ * This file is copied from GetBenchmarks.java and modified for
+ * using SidePluginRepo.
  */
 package org.rocksdb.jmh;
 

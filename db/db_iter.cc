6ea94584ce9c80a3a7fd57c3448793560c18f485
leipeng <peng@topling.cn>
2023-04-22 19:31:13 +0800

    FindValueForCurrentKey: remove useless `Status s`

--- db/db_iter.cc
+++ db/db_iter.cc
@@ -1025,9 +1025,6 @@ bool DBIter::FindValueForCurrentKey() {
     assert(last_key_entry_type == ikey_.type);
   }
 
-  Status s;
-  s.PermitUncheckedError();
-
   switch (last_key_entry_type) {
     case kTypeDeletion:
     case kTypeDeletionWithTimestamp:
@@ -1109,11 +1106,6 @@ bool DBIter::FindValueForCurrentKey() {
           "Unknown value type: " +
           std::to_string(static_cast<unsigned int>(last_key_entry_type)));
       return false;
-  }
-  if (!s.ok()) {
-    valid_ = false;
-    status_ = s;
-    return false;
   }
   valid_ = true;
   return true;

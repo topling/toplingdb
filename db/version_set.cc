f50f0fe987744bbb2e1bcdd839ee521767ced474
Andrew Kryczka <andrew.kryczka2@gmail.com>
2023-12-13 10:37:27 -0800

    Speedup based on pending compaction bytes relative to data size (#12130)
    Summary:
    RocksDB self throttles per-DB compaction parallelism until it detects compaction pressure. The pressure detection based on pending compaction bytes was only comparing against the slowdown trigger (`soft_pending_compaction_bytes_limit`). Online services tend to set that extremely high to avoid stalling at all costs. Perhaps they should have set it to zero, but we never documented that zero disables stalling so I have been telling everyone to increase it for years.
    
    This PR adds pressure detection based on pending compaction bytes relative to the size of bottommost data. The size of bottommost data should be fairly stable and proportional to the logical data size
    
    Pull Request resolved: https://github.com/facebook/rocksdb/pull/12130
    
    Reviewed By: hx235
    
    Differential Revision: D52000746
    
    Pulled By: ajkr
    
    fbshipit-source-id: 7e1fd170901a74c2d4a69266285e3edf6e7631c7

--- db/version_set.cc
+++ db/version_set.cc
@@ -3053,9 +3053,7 @@ void VersionStorageInfo::PrepareForVersionAppend(
   GenerateFileIndexer();
   GenerateLevelFilesBrief();
   GenerateLevel0NonOverlapping();
-  if (!immutable_options.allow_ingest_behind) {
-    GenerateBottommostFiles();
-  }
+  GenerateBottommostFiles();
   GenerateFileLocationIndex();
 }
 

ed4aa1ed1421324e530a76cef82f4bfeefad7c34
Akanksha Mahajan <43301668+akankshamahajan15@users.noreply.github.com>
2023-12-14 13:45:06 -0800

    Fix WRITE_STALL start_time (#12147)
    Summary:
    `Delayed` is set true in two cases. One is when `delay` is specified. Other one is in the `while` loop - https://github.com/facebook/rocksdb/blob/cd21e4e69d76ec4ec3b080c8cdae016ac2309cc5/db/db_impl/db_impl_write.cc#L1876
    However start_time is not initialized in second case, resulting in time_delayed = immutable_db_options_.clock->NowMicros() - 0(start_time);
    
    Pull Request resolved: https://github.com/facebook/rocksdb/pull/12147
    
    Test Plan: Existing CircleCI
    
    Reviewed By: cbi42
    
    Differential Revision: D52173481
    
    Pulled By: akankshamahajan15
    
    fbshipit-source-id: fb9183b24c191d287a1d715346467bee66190f98

--- db/db_impl/db_impl_write.cc
+++ db/db_impl/db_impl_write.cc
@@ -1838,11 +1838,11 @@ Status DBImpl::DelayWrite(uint64_t num_bytes, WriteThread& write_thread,
       delay = 0;
     }
     TEST_SYNC_POINT("DBImpl::DelayWrite:Start");
+    start_time = StopWatch::s_now_micros(immutable_db_options_.clock);
     if (delay > 0) {
       if (write_options.no_slowdown) {
         return Status::Incomplete("Write stall");
       }
-      start_time = StopWatch::s_now_micros(immutable_db_options_.clock);
       TEST_SYNC_POINT("DBImpl::DelayWrite:Sleep");
 
       // Notify write_thread about the stall so it can setup a barrier and

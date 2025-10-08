719d9808ca7330fa04c83f99c3a8b25b059b892c
rockeet <rockeet@gmail.com>
2025-10-08 15:52:20 +0800

    Improve merge-topling.sh

--- merge-topling.sh
+++ merge-topling.sh
@@ -1,3 +1,8 @@
+set -x
+if test -L sideplugin; then
+  rm -f sideplugin
+  git checkout sideplugin
+fi
 if ! test -z "`git status --porcelain -uno`"; then
   echo Workspace is not clean
   git status --porcelain -uno
@@ -12,3 +17,6 @@ git merge memtable_as_log_index
 git checkout --ours java/pom.xml.template
 git add java/pom.xml.template
 git -c core.editor=true merge --continue
+
+rm -rf sideplugin
+ln -s ../toplingdb/sideplugin .
